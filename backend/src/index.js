0ab8a6b24b5ee35a1262e998007f5f437f60f856
Marcel Coelho <marcelfillipe@hotmail.com>
2020-03-24 21:31:21 -0300

    back-end da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,39 @@
+/*
+Metods HTTP:
+   * GET
+   * POST
+   * PUT
+   * DELETE
+*/
+/*
+Tipos de parametros
+   * Query params: parametro nomeados enviado na rota após  "?" (filtro, paginação)
+   * Route params: identificar recursos users/:id
+   * Request body: corpo da requisição, utilizado para criar ou alterar recursos
+*/
+
+/*
+BD
+
+ * SQL: MySQL, SQLite, Postgree, Oracle, Microsoft SQL Server
+ * NoSQL: MongoDB, CouchDB, etc
+
+    Utilizaremos SQLite
+    
+    * Driver: select * from users
+    * Query Builder: table('users').select('*').where()
+
+*/
+
+
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+app.use(cors);
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333);
